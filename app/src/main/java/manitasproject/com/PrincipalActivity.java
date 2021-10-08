ba11f67e44fb7244b83d5a136eb328a0f6feea2b
Cosmo <dalondono2@poligran.edu.co>
2021-10-08 09:32:09 -0500

    Se agrega funcionalidad a los botones del layout principal,

--- app/src/main/java/manitasproject/com/PrincipalActivity.java
+++ app/src/main/java/manitasproject/com/PrincipalActivity.java
@@ -3,26 +3,59 @@ package manitasproject.com;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PrincipalActivity extends AppCompatActivity {
 
+    Button btn_learning,btn_games,btn_options;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prinicipal);
+
+        btn_learning= findViewById(R.id.btn_aprendizaje);
+        btn_games= findViewById(R.id.btn_juegos);
+        btn_options= findViewById(R.id.btn_options);
     }
+
+
+
     public void goToLearning(View view) {
         Intent learning = new Intent(this, LearningActivity.class);
         startActivity(learning);
     }
     public void goToGames(View view) {
-        Intent learning = new Intent(this, GamesActivity.class);
-        startActivity(learning);
+        startActivity(new Intent(this, GamesActivity.class));
     }
     public void goToOptions(View view) {
         Intent learning = new Intent(this, OptionsActivity.class);
         startActivity(learning);
+    }
+
+    public void options(View view) {
+
+        Intent intent = null;
+
+
+        switch (view.getId()){
+            case R.id.btn_aprendizaje:
+                //code
+                intent=new Intent(PrincipalActivity.this,LearningActivity.class);
+                break;
+            case R.id.btn_juegos:
+                //code
+                intent=new Intent(PrincipalActivity.this,GamesActivity.class);
+                break;
+            case R.id.btn_options:
+                //code
+                intent=new Intent(PrincipalActivity.this,OptionsActivity.class);
+                break;
+        }
+        startActivity(intent);
     }
 }
