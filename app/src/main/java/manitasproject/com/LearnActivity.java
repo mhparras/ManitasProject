4943829e30c28d9b6fddf4ab5361dcf061468477
Cosmo <dalondono2@poligran.edu.co>
2021-10-08 19:32:33 -0500

    Se agrega funcionalidad a los botones del layout menu, se corrigen los errores, ya está probrado

--- app/src/main/java/manitasproject/com/LearnActivity.java
+++ app/src/main/java/manitasproject/com/LearnActivity.java
@@ -3,15 +3,12 @@ package manitasproject.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LearnActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_learn);
     }
-
-
 }
--- app/src/main/java/manitasproject/com/MenuActivity.java
+++ app/src/main/java/manitasproject/com/MenuActivity.java
@@ -1,59 +1,40 @@
 package manitasproject.com;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class PrincipalActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     Button btn_learning,btn_games,btn_options;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_prinicipal);
+        setContentView(R.layout.activity_menu);
 
         btn_learning= findViewById(R.id.btn_aprendizaje);
         btn_games= findViewById(R.id.btn_juegos);
         btn_options= findViewById(R.id.btn_options);
     }
 
-
-
-    public void goToLearning(View view) {
-        Intent learning = new Intent(this, LearningActivity.class);
-        startActivity(learning);
-    }
-    public void goToGames(View view) {
-        startActivity(new Intent(this, GamesActivity.class));
-    }
-    public void goToOptions(View view) {
-        Intent learning = new Intent(this, OptionsActivity.class);
-        startActivity(learning);
-    }
-
     public void options(View view) {
-
         Intent intent = null;
-
-
         switch (view.getId()){
             case R.id.btn_aprendizaje:
                 //code
-                intent=new Intent(PrincipalActivity.this,LearningActivity.class);
+                intent=new Intent(MenuActivity.this,LearnActivity.class);
                 break;
             case R.id.btn_juegos:
                 //code
-                intent=new Intent(PrincipalActivity.this,GamesActivity.class);
+                intent=new Intent(MenuActivity.this, GamesActivity.class);
                 break;
             case R.id.btn_options:
                 //code
-                intent=new Intent(PrincipalActivity.this,OptionsActivity.class);
+                intent=new Intent(MenuActivity.this, OptionsActivity.class);
                 break;
         }
         startActivity(intent);
