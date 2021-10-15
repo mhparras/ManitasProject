606cb07d34d4751cdf3760a4dcce56ebfd41f391
Cosmo <dalondono2@poligran.edu.co>
2021-10-15 02:18:10 -0500

    Se ajustan colores basado en la paleta de colores definida, para el layout menu y el layout aprendizaje

--- app/src/main/java/manitasproject/com/MenuActivity.java
+++ app/src/main/java/manitasproject/com/MenuActivity.java
@@ -3,6 +3,7 @@ package manitasproject.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
