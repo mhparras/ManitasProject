9b2b44dfab23c2316ad7d869b411349f6b7437b6
mhparras <mhparras@poligran.edu.co>
2021-11-25 23:31:18 -0500

    Se cambia la vista de la ventana de Juegos.

--- app/src/main/java/manitasproject/com/GamesActivity.java
+++ app/src/main/java/manitasproject/com/GamesActivity.java
@@ -3,12 +3,25 @@ package manitasproject.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GamesActivity extends AppCompatActivity {
+
+    private Button btnVolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
+
+        btnVolver = (Button) findViewById(R.id.btn_volver);
+
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
